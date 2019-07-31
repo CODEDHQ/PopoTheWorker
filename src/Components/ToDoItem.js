7f4d791a695f19d435bec202dd34a1ff48049547
sentientazure <themshary94@gmail.com>
2019-07-31 18:18:37 +0300

    - Added font awesome's regular square icon as the checkbox. Doesn't work yet, gonna make it work next. - Properly displaying the task with the Delete icon on the top right corner (doesn't work yet, gonna make it work later), square checkbox on the far left center vertically, and title and body of the task in the center, expanding and contracting as needed to fill the space, text now wraps if the window is made smaller.

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -1,27 +1,73 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faSquare } from "@fortawesome/free-regular-svg-icons";
 
 class ToDoItem extends Component {
   render() {
     return (
       <div className="list-group-item">
-        <div className="d-flex w-100 justify-content-between">
-          <h5 className="mb-1">{this.props.item.task}</h5>
-          <small>
+        <div className="d-flex justify-content-between">
+          <div class="d-flex align-items-center">
             <FontAwesomeIcon
-              onClick={() => {
-                alert("muwahahahahaha");
-              }}
               style={{ cursor: "pointer" }}
-              icon={faTimes}
+              icon={faSquare}
+              size="2x"
             />
-          </small>
+          </div>
+          <div class="flex-grow-1 p-3 text-wrap">
+            <h5 className="mb-1">{this.props.item.task}</h5>
+            <p className="mb-1">{this.props.item.details}</p>
+          </div>
+          <div>
+            <FontAwesomeIcon style={{ cursor: "pointer" }} icon={faTimes} />
+          </div>
         </div>
-        <p className="mb-1">{this.props.item.details}</p>
       </div>
     );
   }
 }
 
 export default ToDoItem;
+
+/*
+<div className="list-group-item">
+        <div className="d-flex justify-content-between">
+          <div class="w-10 d-flex align-items-center">
+            <FontAwesomeIcon
+              style={{ cursor: "pointer" }}
+              icon={faSquare}
+              size="2x"
+            />
+          </div>
+          <div class="w-100 d-flex justify-content-start">
+            <h5 className="mb-1">{this.props.item.task}</h5>
+            <p className="mb-1">{this.props.item.details}</p>
+          </div>
+          <div class="w-10 d-flex align-items-baseline">
+            <FontAwesomeIcon style={{ cursor: "pointer" }} icon={faTimes} />
+          </div>
+        </div>
+      </div>
+*/
+
+/*
+<div className="container list-group-item">
+        <div className="row">
+          <div className="col-1 d-flex align-items-center">
+            <FontAwesomeIcon
+              style={{ cursor: "pointer" }}
+              icon={faSquare}
+              size="2x"
+            />
+          </div>
+          <div className="col-10">
+            <h5 className="mb-1">{this.props.item.task}</h5>
+            <p className="mb-1">{this.props.item.details}</p>
+          </div>
+          <div className="col-1">
+            <FontAwesomeIcon style={{ cursor: "pointer" }} icon={faTimes} />
+          </div>
+        </div>
+      </div>
+*/
