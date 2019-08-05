a02b6b36f3a9886321bba04d8b422e061d2e4b6f
sentientazure <themshary94@gmail.com>
2019-08-05 16:41:52 +0300

    Added optional label UI, works with multiple labels, uses Bootstrap's Badge that has limited colors.

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -17,8 +17,43 @@ class Task extends Component {
     if (this.props.item.due) {
       dueDate = <small>{this.props.item.due.fromNow()}</small>;
     }
-    return (
-      <div className="list-group-item">
+    let taskHead;
+    if (this.props.item.label)
+      taskHead = (
+        <div>
+          <div className="d-flex justify-content-between">
+            <div className="flex-row">
+              <span className="badge badge-pill badge-primary mr-auto">
+                Primary
+              </span>
+              <span className="badge badge-pill badge-primary mr-auto">
+                Primary
+              </span>
+            </div>
+            <FontAwesomeIcon
+              className="ml-auto"
+              style={{ cursor: "pointer" }}
+              icon={faTimes}
+              onClick={this.deleteTask.bind(this)}
+            />
+          </div>
+          <div className="d-flex justify-content-between">
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon
+                style={{ cursor: "pointer" }}
+                icon={faSquare}
+                size="2x"
+                onClick={this.checkTask.bind(this)}
+              />
+            </div>
+            <div className="flex-grow-1 p-3 text-wrap">
+              <h5 className="mb-1">{this.props.item.task}</h5>
+            </div>
+          </div>
+        </div>
+      );
+    else
+      taskHead = (
         <div className="d-flex justify-content-between">
           <div className="d-flex align-items-center">
             <FontAwesomeIcon
@@ -39,6 +74,10 @@ class Task extends Component {
             />
           </div>
         </div>
+      );
+    return (
+      <div className="list-group-item">
+        {taskHead}
         <p className="mb-1">{this.props.item.details}</p>
         {dueDate}
       </div>
